ca510b827118b5680ce4ccd3f1bbcfad1f34db2e
Chengming Lu <cluster626@gmail.com>
2016-10-18 23:42:26 -0400

    Initial Commit

---
+++ Assets/Scripts/blockSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blockSpawner : MonoBehaviour {
+    public GameObject blockObject;
+	// Use this for initialization
+	void Start () {
+        Debug.Log(blockObject.GetComponent<SpriteRenderer>().bounds.size.x);
+        for (int horizontalPos = 0; horizontalPos <= gameObject.transform.parent.gameObject.GetComponent<RectTransform>().rect.size.x *
+            gameObject.transform.parent.gameObject.GetComponent<Canvas>().scaleFactor; 
+            horizontalPos += Mathf.RoundToInt(blockObject.GetComponent<SpriteRenderer>().bounds.size.x)) {
+            Instantiate(blockObject, new Vector3(horizontalPos, 0, 0), Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
